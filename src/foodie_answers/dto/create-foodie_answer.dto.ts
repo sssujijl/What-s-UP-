4a3bb3d642a7a8c185860bae7ca490cfa30902c3
김성진 <jinni1226@naver.com>
2024-03-31 19:17:02 +0900

    feat/초기단계

--- src/foodie_answers/dto/create-foodie_answer.dto.ts
+++ src/foodie_answers/dto/create-foodie_answer.dto.ts
@@ -1,1 +1,9 @@
-export class CreateFoodieAnswerDto {}
+import { PickType } from "@nestjs/mapped-types";
+import { Foodie_Answer } from "../entities/foodie_answer.entity";
+
+
+export class CreateFoodieAnswerDto extends PickType(Foodie_Answer, [
+    'userId',
+    'content',
+    'images'
+]) {}
--- src/foodie_answers/foodie_answers.controller.ts
+++ src/foodie_answers/foodie_answers.controller.ts
@@ -2,33 +2,38 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { FoodieAnswersService } from './foodie_answers.service';
 import { CreateFoodieAnswerDto } from './dto/create-foodie_answer.dto';
 import { UpdateFoodieAnswerDto } from './dto/update-foodie_answer.dto';
+import { PATH_METADATA } from '@nestjs/common/constants';
 
 @Controller('foodie-answers')
 export class FoodieAnswersController {
   constructor(private readonly foodieAnswersService: FoodieAnswersService) {}
 
-  @Post()
-  create(@Body() createFoodieAnswerDto: CreateFoodieAnswerDto) {
-    return this.foodieAnswersService.create(createFoodieAnswerDto);
+  @Post('create')
+  async createAnswer(
+    @Param("userId") userId: number,
+    @Param('foodieId') foodieId: number,
+    @Body() createFoodieAnswerDto: CreateFoodieAnswerDto
+  ){
+    try {
+      // 1. 게시물에 답글을 작성하려면 일단 로그인된 유저야하고
+      // 2.  
+    } catch (err) {
+      return { message: `${err}`};
+    }
   }
 
-  @Get()
-  findAll() {
-    return this.foodieAnswersService.findAll();
+  @Get() 
+  async findAllAnswers(
+    @Param("foodieId") foodieId: number,
+    @Param("userId") userId: number
+  )
+  {
+    try {
+      await this.
+    } catch (err) {
+      return { message: `${err}`};
+    }
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.foodieAnswersService.findOne(+id);
-  }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFoodieAnswerDto: UpdateFoodieAnswerDto) {
-    return this.foodieAnswersService.update(+id, updateFoodieAnswerDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.foodieAnswersService.remove(+id);
-  }
 }
