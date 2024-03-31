91264ec463771bd435864d960afaa9a8e9858782
이수지 <leesuji1998@naver.com>
2024-03-31 23:50:49 +0900

    follow

--- src/follows/follows.module.ts
+++ src/follows/follows.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FollowsService } from './follows.service';
 import { FollowsController } from './follows.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Follow } from './entities/follow.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Follow]), UsersModule],
   controllers: [FollowsController],
   providers: [FollowsService],
 })
