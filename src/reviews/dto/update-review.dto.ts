1a2a02fc75b3c3354dc49f32a981cd74f8084f71
wltjsl <ljss7291@gmail.com>
2024-04-01 05:20:40 +0900

    review CRUD 대략적으로 작성

--- src/reviews/dto/update-review.dto.ts
+++ src/reviews/dto/update-review.dto.ts
@@ -1,4 +1,43 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReviewDto } from './create-review.dto';
+import { PickType } from '@nestjs/mapped-types';
+import {
+  IsString,
+  IsOptional,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+} from 'class-validator';
+import { Rating } from '../types/rating.types';
+import { Review } from '../entities/review.entity';
 
-export class UpdateReviewDto extends PartialType(CreateReviewDto) {}
+export class UpdateReviewDto extends PickType(Review, ['content']) {
+  /**
+   * 이미지
+   * @example ""
+   */
+  @IsOptional()
+  @IsString()
+  readonly images?: string;
+
+  /**
+   * 내용
+   * @example "별로네"
+   */
+  @IsString()
+  readonly content: string;
+
+  /**
+   * 평점
+   * @example "Rating_1"
+   */
+  @IsOptional()
+  @IsEnum(Rating)
+  readonly rating?: Rating;
+
+  /**
+   * 예약ID
+   * @example "1"
+   */
+  @IsNotEmpty()
+  @IsInt()
+  readonly reservationId: number;
+}
--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,34 +1,128 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  BadRequestException,
+  HttpStatus,
+} from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
+@ApiTags('Reviews')
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
 
+  /**
+   * 리뷰 등록
+   * @param createReviewDto
+   * @returns
+   */
+  @UseGuards(AuthGuard('jwt'))
   @Post()
-  create(@Body() createReviewDto: CreateReviewDto) {
-    return this.reviewsService.create(createReviewDto);
+  async create(@Body() createReviewDto: CreateReviewDto) {
+    try {
+      const data = await this.reviewsService.create(createReviewDto);
+      return {
+        statusCode: HttpStatus.CREATED,
+        message: '리뷰 생성에 성공했습니다.',
+        data,
+      };
+    } catch (error) {
+      throw new BadRequestException('리뷰 생성에 실패했습니다.');
+    }
   }
 
-  @Get()
-  findAll() {
-    return this.reviewsService.findAll();
-  }
-
+  /**
+   * 리뷰 상세 조회
+   * @returns
+   */
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reviewsService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    const data = await this.reviewsService.findOne(+id);
+    if (!data) {
+      return {
+        statusCode: HttpStatus.NOT_FOUND,
+        message: '리뷰를 찾을 수 없습니다.',
+        data,
+      };
+    }
+    return {
+      statusCode: HttpStatus.OK,
+      message: '리뷰 조회에 성공했습니다.',
+      data,
+    };
   }
 
+  /**
+   * 리뷰 수정
+   * @param id
+   * @returns
+   */
+  @UseGuards(AuthGuard('jwt'))
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReviewDto: UpdateReviewDto) {
-    return this.reviewsService.update(+id, updateReviewDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateReviewDto: UpdateReviewDto,
+  ) {
+    try {
+      await this.reviewsService.update(+id, updateReviewDto);
+      return {
+        statusCode: HttpStatus.OK,
+        message: '리뷰 수정에 성공했습니다.',
+      };
+    } catch (error) {
+      throw new BadRequestException('리뷰 수정에 실패했습니다.');
+    }
   }
 
+  /**
+   * 리뷰 목록 조회
+   * @returns
+   */
+  @Get()
+  async findAll() {
+    const data = await this.reviewsService.findAll();
+
+    if (!data.length) {
+      return {
+        statusCode: HttpStatus.NOT_FOUND,
+        message: '보드 목록이 존재하지 않습니다.',
+        data,
+      };
+    }
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: '리뷰 목록 조회에 성공했습니다.',
+      data,
+    };
+  }
+
+  /**
+   * 리뷰 삭제
+   * @param id
+   * @returns
+   */
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reviewsService.remove(+id);
+  async remove(@Param('id') id: string) {
+    try {
+      await this.reviewsService.remove(+id);
+      return {
+        statusCode: HttpStatus.OK,
+        message: '리뷰 삭제에 성공했습니다.',
+      };
+    } catch (error) {
+      throw new BadRequestException('리뷰 삭제에 실패했습니다.');
+    }
   }
 }
