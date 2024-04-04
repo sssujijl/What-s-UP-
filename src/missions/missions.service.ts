751997fc7bf6c876b78111c11a44b8d84550ffc9
이수지 <leesuji1998@naver.com>
2024-04-04 16:44:59 +0900

    feat:mission 생성 중

--- src/missions/missions.service.ts
+++ src/missions/missions.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Mission } from './entities/mission.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Cron } from '@nestjs/schedule';
 import { CreateMissionDto } from './dto/create-mission.dto';
 import { DateTime } from './types/dateTime.type';
@@ -9,7 +9,8 @@ import { DateTime } from './types/dateTime.type';
 @Injectable()
 export class MissionsService {
   constructor (
-    @InjectRepository(Mission) private readonly missionRepository: Repository<Mission>
+    @InjectRepository(Mission) private readonly missionRepository: Repository<Mission>,
+    private dataSource: DataSource,
   ) {}
 
   @Cron(`${getRandomHour()} * * * *`)
@@ -19,21 +20,32 @@ export class MissionsService {
     const currentTime = new Date();
     const currentHour = currentTime.getHours();
     
-    if (currentHour === 7) {
+    if (currentHour === 13) {
       createMissionDto.dateTime = DateTime.TEN_AM;
     } else {
       createMissionDto.dateTime = DateTime.THREE_PN;
     }
 
+    await this.createMission(createMissionDto);
   }
 
-  async createMission() {
+  async createMission(createMissionDto: CreateMissionDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
+    try{
+      const mission = await queryRunner.manager.save(Mission, createMissionDto);
+
+
+    } catch (err) {
+      return { message: `${err}` }
+    }
   }
 }
 
 function getRandomHour(): string {
-  return Math.random() > 0.5 ? '07' : '12';
+  return Math.random() > 0.5 ? '13' : '18';
 }
 
 function getRandomAttendees(): number {
